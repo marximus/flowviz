aa835eb3269601a31bc6f791ad77c5f4585771fe
Maurice Marx <momomarx@gmail.com>
2018-04-11 23:20:32 -0400

    Add ability to color either image or sequence of images

--- flowviz/color_flow.py
+++ flowviz/color_flow.py
@@ -19,33 +19,42 @@ def _unknown_flow(u, v):
 """
 color_flow.cpp
 """
-def motion_to_color(motim, maxmotion=None, verbose=False):
+def motion_to_color(inmotim, maxmotion=None, verbose=False):
     """
 
     Parameters
     ----------
-    motim : ndarray, dtype float, shape (height, width, 2)
-        Motion image of vectors.
-    maxmotion :
+    inmotim : ndarray, dtype float, shape (height, width, 2) OR (length, height, width, 2)
+        Array of vector components. Can be either a single array or a sequence of arrays of vector components.
+    maxmotion : float
+        Maximum value to normalize by.
 
     Returns
     -------
-    colim : ndarray, dtype uint8, shape (height, width, 3)
+    colim : ndarray, shape (height, width, 3) or (length, height, width, 3), dtype uint8
         Colored image.
     """
-    height, width, _ = motim.shape
-    colim = np.zeros((height, width, 3), dtype=np.uint8)
+    if inmotim.ndim == 3:
+        motim = inmotim[None, ...]
+    else:
+        motim = inmotim
+
+    if motim.ndim != 4 or motim.shape[-1] != 2:
+        quit('motim must be a (length, height, width, 2) array')
+
+    length, height, width, _ = motim.shape
+    colim = np.zeros((length, height, width, 3), dtype=np.uint8)
 
     # determine motion range
-    maxx, maxy = motim[:, :, 0].max(), motim[:, :, 1].max()
-    minx, miny = motim[:, :, 0].min(), motim[:, :, 1].min()
-    fx = motim[:, :, 0]
-    fy = motim[:, :, 1]
+    # maxx, maxy = motim[..., 0].max(), motim[..., 1].max()
+    # minx, miny = motim[..., 0].min(), motim[..., 1].min()
+    fx = motim[:, :, :, 0]
+    fy = motim[:, :, :, 1]
     rad = np.sqrt(fx**2 + fy**2)
     maxrad = rad.max()
-    print("max motion: {:.4f}   motion range: u = {:.3f} .. {:.3f};  v = {:.3f} .. {:.3f}".format(
-        maxrad, minx, maxx, miny, maxy
-    ))
+    # print("max motion: {:.4f}   motion range: u = {:.3f} .. {:.3f};  v = {:.3f} .. {:.3f}".format(
+    #     maxrad, minx, maxx, miny, maxy
+    # ))
 
     if maxmotion is not None:
         maxrad = maxmotion
@@ -56,23 +65,16 @@ def motion_to_color(motim, maxmotion=None, verbose=False):
     if verbose:
         print("normalizing by {}".format(maxrad))
 
-    colorcode.compute_color(fx/maxrad, fy/maxrad, colim)
+    for i in range(length):
+        fx = motim[i, :, :, 0]
+        fy = motim[i, :, :, 1]
+        colorcode.compute_color(fx/maxrad, fy/maxrad, colim[i])
 
+    fx = motim[:, :, :, 0]
+    fy = motim[:, :, :, 1]
     idx = _unknown_flow(fx, fy)
     colim[idx] = 0
 
+    if inmotim.ndim == 3:
+        return colim[0]
     return colim
-
-
-def main(flowname, maxmotion=None):
-    flow = np.load(flowname)
-    flow = np.moveaxis(flow, 0, -1)
-    im = flow[0]
-
-    outim = motion_to_color(im, maxmotion)
-
-    # save to file
-    from matplotlib.image import imsave
-    imsave('color-flow.png', outim, dpi=100)
-
-
